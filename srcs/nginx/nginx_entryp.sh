4efe2a554569eda26e305289d22a8fc87aef01b8
Jose <jleonardola@gmail.com>
2025-08-10 00:06:45 +0200

    Pretty naked nginx

--- srcs/nginx/nginx_entryp.sh
+++ srcs/nginx/nginx_entryp.sh
@@ -3,7 +3,13 @@ set -ex
 
 envsubst '${SERVER_NAME}' < /etc/nginx/mysite.conf.to_expand > /etc/nginx/sites-enabled/mysite.conf
 
-cd /etc/nginx/ssl && bash sign_cert.sh && test -f  && test -f /etc/nginx/ssl/${SERVER_NAME}.key #Exec script with ENVs loaded
+cd /etc/nginx/ssl 
+
+SUBJECT="/C=${SSL_C}/ST=${SSL_ST}/L=${SSL_LO}/O=${SSL_OP}/OU=${SSL_OU}/CN=${SERVER_NAME}" 
+echo "Signing certificate with server name ${SUBJECT}";
+openssl req -x509 -newkey rsa:4086 -keyout ${SERVER_NAME}.key -out ${SERVER_NAME}.crt -days 666 -nodes -subj "$SUBJECT"
+
+cd
 
 if [ ! -f "/etc/nginx/ssl/${SERVER_NAME}.crt" ]; then
   echo "/etc/nginx/ssl/${SERVER_NAME}.crt not found, aborting."
@@ -16,5 +22,7 @@ if [ ! -f "/etc/nginx/ssl/${SERVER_NAME}.key" ]; then
 fi
 
 nginx -t
+
+rm -f /var/www/html/index.nginx-debian.html
 
 exec "$@" # Very important otherwise the CMD on the dockerfile won't really run. It also makes that CMD run as PID 1. Which is good. For reasons.
--- srcs/nginx/sign_cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SUBJECT="/C=${SSL_C}/ST=${SSL_ST}/L=${SSL_LO}/O=${SSL_OP}/OU=${SSL_OU}/CN=${SERVER_NAME}" 
-echo "Signing certificate with server name ${SUBJECT}";
-openssl req -x509 -newkey rsa:4086 -keyout ${SERVER_NAME}.key -out ${SERVER_NAME}.crt -days 666 -nodes -subj "$SUBJECT"
-
-echo "$SUBJECT " > SIGH
