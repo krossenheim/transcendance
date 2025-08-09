9dc2881c5da6d2062face7590d054773c90b2076
Jose <jleonardola@gmail.com>
2025-08-09 23:39:08 +0200

    Whoops. We pray for inception

--- srcs/nginx/nginx_entryp.sh
+++ srcs/nginx/nginx_entryp.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -ex
 
-cd /etc/nginx/ssl && bash sign_cert.sh && test -f /etc/nginx/ssl/*.crt && test -f /etc/nginx/ssl/*.key #Exec script with ENVs loaded
+envsubst '${SERVER_NAME}' < /etc/nginx/mysite.conf.to_expand > /etc/nginx/sites-enabled/mysite.conf
+
+cd /etc/nginx/ssl && bash sign_cert.sh && test -f  && test -f /etc/nginx/ssl/${SERVER_NAME}.key #Exec script with ENVs loaded
+
+if [ ! -f "/etc/nginx/ssl/${SERVER_NAME}.crt" ]; then
+  echo "/etc/nginx/ssl/${SERVER_NAME}.crt not found, aborting."
+  exit 1
+fi
+
+if [ ! -f "/etc/nginx/ssl/${SERVER_NAME}.key" ]; then
+  echo "/etc/nginx/ssl/${SERVER_NAME}.key  not found, aborting."
+  exit 1
+fi
 
 nginx -t
 
-exec "$@" # Very important otherwise the CMD on the dockerfile won't really run. It also makes that CMD run as PID 1. Which is good.
+exec "$@" # Very important otherwise the CMD on the dockerfile won't really run. It also makes that CMD run as PID 1. Which is good. For reasons.
--- srcs/nginx/sign_cert.sh
+++ srcs/nginx/sign_cert.sh
@@ -2,6 +2,6 @@
 
 SUBJECT="/C=${SSL_C}/ST=${SSL_ST}/L=${SSL_LO}/O=${SSL_OP}/OU=${SSL_OU}/CN=${SERVER_NAME}" 
 echo "Signing certificate with server name ${SUBJECT}";
-openssl req -x509 -newkey rsa:4086 -keyout login.42.fr.key -out login.42.fr.crt -days 666 -nodes -subj "$SUBJECT"
+openssl req -x509 -newkey rsa:4086 -keyout ${SERVER_NAME}.key -out ${SERVER_NAME}.crt -days 666 -nodes -subj "$SUBJECT"
 
 echo "$SUBJECT " > SIGH
