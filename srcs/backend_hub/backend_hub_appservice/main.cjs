ee450688f972fa65cd49eb177cd4d24cae14f41d
Jan Bakker <jbakker@LordOfTheFlies.bogus.com>
2025-09-07 19:23:11 +0200

    Add routing to fastify main container

--- srcs/backend_hub/backend_hub_appservice/main.cjs
+++ srcs/backend_hub/backend_hub_appservice/main.cjs
@@ -197,6 +197,43 @@ fastify.register(async function ()
   })
 })
 
+async function proxyRequest(req, reply, method, url) {
+  console.log(`Proxying ${method} request to: ${url}`);
+	try {
+    const response = await axios({
+      method,
+      url,
+      headers: {
+        ...req.headers,
+        host: undefined,
+        connection: undefined,
+      },
+      data: req.body,
+      params: req.query,
+      validateStatus: () => true,
+    });
+    reply.code(response.status).send(response.data);
+	} catch (error) {
+		console.error('Error proxying request:', error);
+		reply.code(500).send({ error: 'Internal Server Error: ' + error.message });
+	}
+}
+
+fastify.all('/api/:dest/public/*', async (req, reply) => {
+	const { dest } = req.params;
+	const restOfUrl = req.url.replace(`${dest}/`, '');
+	const url = `http://${dest}:3000${restOfUrl}`;
+  await proxyRequest(req, reply, req.method, url);
+});
+
+fastify.all('/api/:dest/*', async (req, reply) => {
+	const { dest } = req.params;
+	const restOfUrl = req.url.replace(`${dest}/`, '');
+	const url = `http://${dest}:3000${restOfUrl}`;
+	await proxyRequest(req, reply, req.method, url);
+});
+
+
 fastify.register(async function () 
 {
     fastify.route({
