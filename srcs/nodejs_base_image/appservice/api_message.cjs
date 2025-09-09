a6439b878e2939c580e97b30ed73deacf6b02600
Jose <jleonardola@gmail.com>
2025-09-09 20:04:43 +0200

    Api message .cjs worky

--- srcs/nodejs_base_image/appservice/api_message.cjs
+++ srcs/nodejs_base_image/appservice/api_message.cjs
@@ -1,16 +1,39 @@
 const { containerNames } = require('/appservice/_container_names.cjs')
 
+const allowedStatusValues = ["Success", "Error"];
+
+const allowedDestination = ["Internal-Container", "External-Client"];
+
 class ApiMessage {
-	constructor(status, containerFrom) 
+    #status
+    #destination
+    #containerFrom
+	constructor(status, containerFrom, destination, payload) 
 	{
 		this.status = status;
 		this.containerFrom = containerFrom;
-		this.messages = new FixedSizeList(20);
+        this.destination = destination;
+		this.payload = payload;
 	}
+
+
+    get destination()
+    {
+        return (this.#destination);
+    }
+
+    set destination(value)
+    {
+        if (typeof value !== "string" || !allowedDestination.includes(value)) 
+            {
+                throw new Error("Destination must be one of: " + allowedDestination.join(", "));
+            }
+        this.#destination = value;
+    }
 
     get status()
     {
-        return (this._status);
+        return (this.#status);
     }
 
     set status(value)
@@ -19,21 +42,21 @@ class ApiMessage {
             {
                 throw new Error("Status must be one of: " + allowedStatusValues.join(", "));
             }
-        this._status = value;
+        this.#status = value;
     }
 
     get containerFrom()
     {
-        return (this._status);
+        return (this.#containerFrom);
     }
 
     set containerFrom(value)
     {
         if (typeof value !== "string" || !containerNames.includes(value)) 
             {
-                throw new Error("Status must be one of: " + allowedStatusValues.join(", "));
+                throw new Error("Status must be one of: " + containerNames.join(", "));
             }
-        this._containerFrom = value;
+        this.#containerFrom = value;
     }
     
 }
