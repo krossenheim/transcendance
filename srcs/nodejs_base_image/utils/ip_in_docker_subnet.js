6b14edf4d1926f91464d23dc1230cf34204b06d3
user <user@inception.myguest.virtualbox.org>
2025-09-23 16:33:13 +0200

    Seems im relatively able to debug .ts

--- srcs/nodejs_base_image/utils/ip_in_docker_subnet.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function ipInDockerSubnet(customNginxHeaderFromDockerSubnet) {
-    return (customNginxHeaderFromDockerSubnet != "0");
-}
-module.exports = ipInDockerSubnet;
-//# sourceMappingURL=ip_in_docker_subnet.js.map
