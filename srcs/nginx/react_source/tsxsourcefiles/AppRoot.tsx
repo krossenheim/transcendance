dba31cafa5a6910df29a8aa8db7beb10da2ad98b
Jose <jleonardola@gmail.com>
2025-10-14 17:36:13 +0200

    js to tsx ongoing

---
+++ srcs/nginx/react_source/tsxsourcefiles/AppRoot.tsx
@@ -0,0 +1,34 @@
+import RegisterComponent from "./registerComponent";
+import SocketComponent from "./socketComponent";
+import LoginComponent from "./loginComponent";
+import ChatComponent from "./chatComponent";
+import PongComponent from "./pongComponent";
+import React, { useState } from "react";
+import { AuthResponseType } from "../../../nodejs_base_image/utils/api/service/auth/loginResponse";
+
+export default function AppRoot() {
+  const [authResponse, setAuthResponse] = useState<AuthResponseType | null>(
+    null
+  );
+
+  function logInOrRegistered(varTypeAuthResponse: AuthResponseType) {
+    setAuthResponse(varTypeAuthResponse);
+  }
+
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      {authResponse ? (
+        <SocketComponent AuthResponseObject={authResponse} />
+      ) : (
+        <div className="flex gap-8 justify-center items-start">
+          <div className="w-1/2">
+            <LoginComponent onLoginSuccess={logInOrRegistered} />
+          </div>{" "}
+          <div className="w-1/2">
+            <RegisterComponent whenCompletedSuccesfully={logInOrRegistered} />
+          </div>
+        </div>
+      )}
+    </main>
+  );
+}
