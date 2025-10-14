ca085d8be9b7a22a08fd7f6ba7cea50be39295d3
Jose <jleonardola@gmail.com>
2025-10-14 18:36:48 +0200

    Add a little type for registerRoute

--- srcs/nodejs_base_image/utils/api/service/common/fastify.ts
+++ srcs/nodejs_base_image/utils/api/service/common/fastify.ts
@@ -15,43 +15,48 @@ export function createFastify(options = { logger: true }): FastifyInstance {
 }
 
 type ReplyOf<T extends HTTPRouteDef> = Omit<FastifyReply, "status"> & {
-	status<Code extends keyof T["schema"]["response"] & number>(
-		code: Code
-	): Omit<FastifyReply, "send"> & {
-		send(payload: z.infer<T["schema"]["response"][Code]>): void;
-	};
+  status<Code extends keyof T["schema"]["response"] & number>(
+    code: Code
+  ): Omit<FastifyReply, "send"> & {
+    send(payload: z.infer<T["schema"]["response"][Code]>): void;
+  };
+};
+
+export type ExtractZodSchema<T extends { schema?: any }> = {
+  Body: T["schema"] extends { body: z.ZodTypeAny }
+    ? z.infer<T["schema"]["body"]>
+    : never;
+
+  Querystring: T["schema"] extends { query: z.ZodTypeAny }
+    ? z.infer<T["schema"]["query"]>
+    : never;
+
+  Params: T["schema"] extends { params: z.ZodTypeAny }
+    ? z.infer<T["schema"]["params"]>
+    : never;
 };
 
 export function registerRoute<T extends HTTPRouteDef>(
-	fastify: FastifyInstance,
-	route: T,
-	handler: (
-		req: FastifyRequest<
-			{
-				Body: T["schema"] extends { body: z.ZodTypeAny }
-					? z.infer<T["schema"]["body"]>
-					: never;
-				Querystring: T["schema"] extends { query: z.ZodTypeAny }
-					? z.infer<T["schema"]["query"]>
-					: never;
-				Params: T["schema"] extends { params: z.ZodTypeAny }
-					? z.infer<T["schema"]["params"]>
-					: never;
-			}
-		>,
-		reply: ReplyOf<T>
-	) => Promise<void>
+  fastify: FastifyInstance,
+  route: T,
+  handler: (
+    req: FastifyRequest<ExtractZodSchema<T>>,
+    reply: ReplyOf<T>
+  ) => Promise<void>
 ) {
-	const method = (route.method ?? "POST").toLowerCase() as
-		| "get"
-		| "post"
-		| "put"
-		| "delete";
+  const method = (route.method ?? "POST").toLowerCase() as
+    | "get"
+    | "post"
+    | "put"
+    | "delete";
 
-	fastify[method](route.endpoint, { schema: route.schema }, async (req, reply) => {
-		await handler(req as any, reply as any);
-	});
+  fastify[method](
+    route.endpoint,
+    { schema: route.schema },
+    async (req, reply) => {
+      await handler(req as any, reply as any);
+    }
+  );
 }
-
 
 export default { createFastify, registerRoute };
