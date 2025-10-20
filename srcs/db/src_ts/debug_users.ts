5424552f320b3f89675fbf2b215bba261292cb70
Jose <jleonardola@gmail.com>
2025-10-20 16:27:25 +0200

    Debug for pong for specific accounts

--- srcs/db/src_ts/debug_users.ts
+++ srcs/db/src_ts/debug_users.ts
@@ -3,21 +3,19 @@ import { hashPassword } from "./routes/users.js";
 export async function makedebugusers(userService: any): Promise<void> {
   const hashed = await hashPassword("apassword");
   const safedebugpasswords = [];
-  safedebugpasswords.push(
-    userService.createNewUser("userone", "boasgus@bogii.coms", hashed, false)
+
+  userService.createNewUser("userone", "boasgus@bogii.coms", hashed, false);
+  userService.createNewUser("usertwo", "bogasdus@bog2ii.coms", hashed, false);
+  userService.createNewUser(
+    "userthree",
+    "bogdssus@bog2isi.coms",
+    hashed,
+    false
   );
-  safedebugpasswords.push(
-    userService.createNewUser("usertwo", "bogasdus@bog2ii.coms", hashed, false)
-  );
-  safedebugpasswords.push(
-    userService.createNewUser(
-      "userthree",
-      "bogdssus@bog2isi.coms",
-      hashed,
-      false
-    )
-  );
-  safedebugpasswords.push(
-    userService.createNewUser("guest", "bogdssus@bog2isi.coms", hashed, false)
-  );
+  userService.createNewUser("pong", "pong@bogii.coms", hashed, false);
+  userService.createNewUser("pong1", "po1ng@bogii.coms", hashed, false);
+  userService.createNewUser("pong2", "po2ng@bogii.coms", hashed, false);
+  userService.createNewUser("pong3", "pon3g@bogii.coms", hashed, false);
+  userService.createNewUser("pong4", "pon4g@bogii.coms", hashed, false);
+  userService.createNewUser("guest", "bogdssus@bog2isi.coms", hashed, false);
 }
