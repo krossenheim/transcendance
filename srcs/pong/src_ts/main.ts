0c2c8394a0ce0a8ba2cc9e093eed8eb5e409b4bb
Jose <jleonardola@gmail.com>
2025-10-20 16:07:31 +0200

    No more pong

--- srcs/pong/src_ts/main.ts
+++ srcs/pong/src_ts/main.ts
@@ -77,8 +77,8 @@ socket.registerEvent(
   }
 );
 
-singletonPong.startGame({
-  user_id: 2,
-  funcId: "/api/start_game",
-  payload: { player_list: [2, 3, 4, 1, 5] },
-});
+// singletonPong.startGame({
+//   user_id: 2,
+//   funcId: "/api/start_game",
+//   payload: { player_list: [2, 3, 4, 1, 5] },
+// });
