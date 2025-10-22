e2836c74cec068a1b1e3904282287e61c169e450
Jose <jleonardola@gmail.com>
2025-10-22 20:16:36 +0200

    Forgot to add dis file

---
+++ srcs/nodejs_base_image/utils/users_on_hub_online_status.ts
@@ -0,0 +1,28 @@
+import { int_url } from "./api/service/common/endpoints";
+import { Result } from "./api/service/common/result";
+import { FullUserType } from "./api/service/db/user";
+import { OurSocket } from "./socket_to_hub";
+
+function getCompleteOnlineUserList(socket: OurSocket) {
+  
+}
+class OnlineUserStatus {
+  private user_list: Array<FullUserType>;
+
+  constructor(socket: OurSocket) {
+    socket.registerEvent(
+      int_url.ws.serviceProviders.userConnected,
+      async (body) => {
+        return Result.Ok(null);
+      }
+    );
+    socket.registerEvent(
+      int_url.ws.serviceProviders.userDisconnected,
+      async (body) => {
+        return Result.Ok(null);
+      }
+    );
+
+    user_list = getCompleteOnlineUserList(socket);
+  }
+}
