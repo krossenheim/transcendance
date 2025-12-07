0ed64d9262d5e37973ea4e6745759f30481abc75
Thijs <thivan-d@student.codam.nl>
2025-12-07 23:33:21 +0100

    back/forward

--- srcs/nginx/react_source/tsxsourcefiles/main.tsx
+++ srcs/nginx/react_source/tsxsourcefiles/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import AppRoot from "./AppRoot";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppRoot />
+    <BrowserRouter>
+      <AppRoot />
+    </BrowserRouter>
   </React.StrictMode>
 );
